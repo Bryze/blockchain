87990db857cc896a25e7c178b6e89e6b6cd29c55
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-11 23:08:46 +0530

    added testing code for firebase ui

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './blockchain/main';
+import './auth/authentication';
+import * as firebaseui from 'firebaseui';
+
+let uiConfig = {
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        Authentication.getInstance().getFirebaseObject().auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+};
+console.log(...uiConfig);
+// Initialize the FirebaseUI Widget using Firebase.
+let ui = new firebaseui.auth.AuthUI(Authentication.getInstance().getFirebaseObject().auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
