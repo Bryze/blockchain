9e2843c8323990d598e78d1988281f983c623ca4
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-12 22:07:57 +0530

    added default route group component for handling all routes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './blockchain/main';
 import './auth/authentication';
-import * as firebaseui from 'firebaseui';
+import {BrowserRouter, Route} from 'react-router-dom';
+import ProjectSelector from "./components/ProjectSelector";
 
-let uiConfig = {
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        Authentication.getInstance().getFirebaseObject().auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>'
-};
-console.log(...uiConfig);
-// Initialize the FirebaseUI Widget using Firebase.
-let ui = new firebaseui.auth.AuthUI(Authentication.getInstance().getFirebaseObject().auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+const RouteGroups = () => (
+    <BrowserRouter>
+        <Route path="/" component={ProjectSelector}/>
+    </BrowserRouter>
+);
+ReactDOM.render(<RouteGroups />, document.getElementById('app'));
