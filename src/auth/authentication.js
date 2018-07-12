03ce32e24a77efee8fa41754b84da5132af6996b
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-12 22:09:11 +0530

    encapsulated all firebaseui related things into authentication class

--- src/auth/authentication.js
+++ src/auth/authentication.js
@@ -2,6 +2,7 @@
  * Created by Aseem on 11-07-2018.
  */
 import Credentials from '../firebase/credentials';
+import * as firebaseui from 'firebaseui';
 
 let Authentication = (function () {
     let instance = null;
@@ -18,6 +19,17 @@ let Authentication = (function () {
 
 class Auth extends Credentials {
 
+    createFirebaseLogin(redirectUrl, div, serviceUrl = '') {
+        let config = {
+            signInSuccessUrl: redirectUrl,
+            signInOptions: [
+                this.getFirebaseObject().auth.EmailAuthProvider.PROVIDER_ID,
+            ],
+            tosUrl: serviceUrl
+        };
+        let ui = new firebaseui.auth.AuthUI(this.getFirebaseObject().auth());
+        ui.start(div, config);
+    }
 }
 
 window.Authentication = Authentication;
