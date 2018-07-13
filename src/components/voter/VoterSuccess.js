36d5e36cbd1e239bdb1b0ab57ae8d4e2d69d99c5
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-14 00:08:00 +0530

    post voter login success screen

---
+++ src/components/voter/VoterSuccess.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Aseem on 13-07-2018.
+ */
+import React, {Component} from 'react';
+import Base from './Base';
+
+class VoterSuccess extends Base {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default VoterSuccess;
