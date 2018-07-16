974a38c3c1f4f93156a30ea4fa6041fdbcc119c6
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-16 23:12:41 +0530

    an event is now raised wheneven user object is initialized completely

--- src/components/voter/Base.js
+++ src/components/voter/Base.js
@@ -7,7 +7,12 @@ class Base extends Component {
     constructor(props) {
         super(props);
         Authentication.getInstance().getFirebaseObject().auth().onAuthStateChanged((user) => {
+            if(user) {
+                Authentication.getInstance().setUser(user);
+                Authentication.getInstance().raiseEvent('firebaseUserInit',user);
+            }
             if(!user) {
+                Authentication.getInstance().resetUser();
                 props.history.push('/');
             }
         })
