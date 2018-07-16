cc159528dba6e222e080e1ec6bc39936a9a27710
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-16 23:13:15 +0530

    added wrappers for event handling

--- src/auth/authentication.js
+++ src/auth/authentication.js
@@ -3,6 +3,7 @@
  */
 import Credentials from '../firebase/credentials';
 import * as firebaseui from 'firebaseui';
+let EventHandler = require('eventhandler');
 
 let Authentication = (function () {
     let instance = null;
@@ -19,6 +20,12 @@ let Authentication = (function () {
 
 class Auth extends Credentials {
 
+    constructor() {
+        super();
+        this.user = null;
+        this.eventHandler = new EventHandler();
+    }
+
     createFirebaseLogin(redirectUrl, div, serviceUrl = '') {
         let config = {
             signInSuccessUrl: redirectUrl,
@@ -30,6 +37,33 @@ class Auth extends Credentials {
         };
         let ui = new firebaseui.auth.AuthUI(this.getFirebaseObject().auth());
         ui.start(div, config);
+    }
+
+    setUser(user) {
+        if(!this.user) {
+            this.user = user;
+        }
+        return this;
+    }
+
+    getUser() {
+        return this.user;
+    }
+
+    resetUser() {
+        this.user = null;
+        return this;
+    }
+
+    raiseEvent(eventName,data) {
+        this.eventHandler.emit(eventName,data);
+        return this;
+    }
+
+    subscribeEvent(eventName,callback) {
+        this.eventHandler.on(eventName, function(response) {
+            callback(response);
+        });
     }
 }
 
