be4b7287ca461af59e9807264678fd5499dea016
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-16 23:11:51 +0530

    added parent root creation if not present at successful login

--- src/components/voter/VoterSuccess.js
+++ src/components/voter/VoterSuccess.js
@@ -5,8 +5,25 @@ import React, {Component} from 'react';
 import Base from './Base';
 
 class VoterSuccess extends Base {
-    constructor(props) {
-        super(props);
+
+    componentDidMount() {
+        Authentication.getInstance().subscribeEvent('firebaseUserInit', function(response) {
+            let uid = response.uid;
+            Authentication.getInstance().getFDBO().ref('/voting').once('value').then(
+                snapshot => {
+                    if(!snapshot.val()) {
+                        Authentication.getInstance().getFDBO().ref().child('voting').set(
+                            {
+                                'blockChain': 0,
+                                'pendingTransactions': 0,
+                                'miners': 0,
+                                'roles': 0
+                            }
+                        );
+                    }
+                }
+            )
+        });
     }
 
     render() {
