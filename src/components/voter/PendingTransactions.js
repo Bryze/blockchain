9b1fe718e3aea118cd84ffa8ebbccfc7992ce571
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-23 23:26:45 +0530

    pending transaction screen added

---
+++ src/components/voter/PendingTransactions.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Aseem on 23-07-2018.
+ */
+import React, {Component} from 'react';
+
+class PendingTransactions extends Component
+{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                In Pending Transactions
+            </div>
+        )
+    }
+}
+
+export default PendingTransactions;
