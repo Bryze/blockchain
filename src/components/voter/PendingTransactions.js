5451e6426ab8a5ed662a9d5aa3eff07f15b6907b
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-25 21:27:45 +0530

    added scope for fetching pending transactions

--- src/components/voter/PendingTransactions.js
+++ src/components/voter/PendingTransactions.js
@@ -2,17 +2,52 @@
  * Created by Aseem on 23-07-2018.
  */
 import React, {Component} from 'react';
+import Button from "@material-ui/core/Button";
 
 class PendingTransactions extends Component
 {
     constructor(props) {
         super(props);
+        this.state = {
+            hasPendingTransactions: false
+        }
+    }
+
+    fetchPendingTransactions = () => {
+      Authentication.getInstance().getFDBO().ref('/voting/pendingTransactions').once('value').then(
+          snapshot => {
+             if(snapshot.val()!==0) {
+                 Authentication.getInstance().getFDBO().ref('/voting/miners/').child(this.props.uid).update({
+                     pendingTransactions: snapshot.val()
+                 });
+                 this.setState({
+                    hasPendingTransactions: true
+                 });
+                 this.props.updatePendingTransactions(true);
+                 Authentication.getInstance().getFDBO().ref('/voting').update({pendingTransactions:0});
+             }
+          }
+      );
+    };
+
+    componentDidMount() {
+        Authentication.getInstance().getFDBO().ref('/voting/miners').child(this.props.uid).once('value').then(
+            snapshot => {
+                if(snapshot.val().pendingTransactions!==0) {
+                    this.setState({
+                        hasPendingTransactions: true
+                    });
+                    this.props.updatePendingTransactions(true);
+                }
+            }
+        )
     }
 
     render() {
         return (
             <div>
-                In Pending Transactions
+                {!this.state.hasPendingTransactions && <Button variant="contained" onClick={this.fetchPendingTransactions}>Fetch Pending Transactions</Button>}
+                {this.state.hasPendingTransactions && <div>Mine your previous pending transactions first</div>}
             </div>
         )
     }
