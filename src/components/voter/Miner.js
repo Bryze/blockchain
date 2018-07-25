6d22cae348d1129c415b74effd6f68ab908c9c31
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-25 21:26:30 +0530

    added mineblock screen

--- src/components/voter/Miner.js
+++ src/components/voter/Miner.js
@@ -4,6 +4,7 @@
 import React, {Component} from 'react';
 import Button from "@material-ui/core//Button";
 import PendingTransactions from "./PendingTransactions";
+import MineBlock from "./MineBlock";
 
 class Miner extends Component {
     constructor(props) {
@@ -12,7 +13,8 @@ class Miner extends Component {
             displayText: 'Fetching miner profile..',
             isLoading: true,
             minerProfile: null,
-            isProfileButtonClicked: false
+            isProfileButtonClicked: false,
+            hasPendingTransactions: false
         };
     }
 
@@ -59,17 +61,24 @@ class Miner extends Component {
         })
     };
 
+    updatePendingTransactionStatus = (status) => {
+        this.setState({
+            hasPendingTransactions: status
+        })
+    };
+
     render() {
         return (
             <div>
                 {this.state.displayText}
                 {!this.state.isLoadign && <Button variant="contained" onClick={this.handleProfileButtonClick}>View Profile</Button>}
                 {!this.state.isLoading && this.state.isProfileButtonClicked &&
-                    Object.keys(this.state.minerProfile).map((keys,index) => {
-                    return <div key={index}>{keys+" "+this.state.minerProfile[keys]}</div>
-                    })
+                Object.keys(this.state.minerProfile).map((keys, index) => {
+                    return <div key={index}>{keys + " " + this.state.minerProfile[keys]}</div>
+                })
                 }
-                {!this.state.isLoading && <PendingTransactions {...this.props} />}
+                {!this.state.isLoading && <PendingTransactions {...this.props} updatePendingTransactions={this.updatePendingTransactionStatus}/>}
+                {this.state.hasPendingTransactions && <MineBlock {...this.props} />}
             </div>
         )
     }
