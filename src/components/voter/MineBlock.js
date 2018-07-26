4214faf4f7b7dbda4ffc80356215721d95907f3c
Aseem Upadhyay <aseem2301@gmail.com>
2018-07-26 21:46:18 +0530

    added pendng transaction processor

--- src/components/voter/MineBlock.js
+++ src/components/voter/MineBlock.js
@@ -3,27 +3,46 @@
  */
 import React, {Component} from 'react';
 
-class MineBlock extends Component
-{
+class MineBlock extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            statusText: 'Checking for transactions',
+            hashesComputed: 0
+        }
     }
 
     processPendingTransactions = (data) => {
-        console.log(data,'data');
+        let coin = App.getInstance();
+        Object.keys(data).forEach(keys => {
+            coin.getBlockChainObject().createTransaction(
+                coin.getNewTransactionObject(data[keys].votedBy, data[keys].votedTo, 1)
+            );
+        });
+        coin.getBlockChainObject().minePendingTransactions(this.props.uid);
+        this.setState({
+            statusText: 'Block mined!',
+            hashesComputed: coin.getBlockChainObject().getHashesComputed()
+        });
+        Authentication.getInstance().getFDBO().ref('/voting').update({
+            blockChain: coin.getBlockChainObject().getChain()
+        });
     };
 
     componentDidMount() {
         Authentication.getInstance().getFDBO().ref('/voting/blockChain').once('value').then(
             snapshot => {
-                if(!snapshot.val()) {
-                    Authentication.getInstance().getFDBO().ref('/voting/miners/'+this.props.uid+'/pendingTransactions').once('value').then(
+                if (!snapshot.val()) {
+                    Authentication.getInstance().getFDBO().ref('/voting/miners/' + this.props.uid + '/pendingTransactions').once('value').then(
                         data => {
+                            this.setState({
+                                statusText: 'Transactions fetched. Mining transactions'
+                            });
                             this.processPendingTransactions(data.val());
                         }
                     );
                 } else {
-
+                    //sync already existing blockchain object
                 }
             }
         )
@@ -31,7 +50,10 @@ class MineBlock extends Component
 
     render() {
         return (
-            <div>Block mined</div>
+            <div>
+                <div>{this.state.statusText}</div>
+                <div>{this.state.hashesComputed + " hashes computed."}</div>
+            </div>
         )
     }
 }
